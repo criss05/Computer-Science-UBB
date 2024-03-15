7b09ff78f674fdcca46157202cdae3f257785527
criss05 <barnutcristiana05@gmail.com>
2024-03-15 10:53:25 +0200

    Start Done

---
+++ LogicaComputationala/operations with difrent bases/start.py
@@ -0,0 +1,11 @@
+# Project made by Bărnuț Cristiana
+import UI
+import test_data
+
+test_data.test_addition()
+test_data.test_subtraction()
+test_data.test_multiplication()
+test_data.test_addition()
+
+UI.print_menu()
+UI.menu_application()
