a9769c2711b47928441c6b45cf670fc6d34e3f29
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-07-28 21:06:03 +0300

    DBMS first lab
    Simple example to get used with the env

---
+++ Second Year/Database_Management_System/Lab0/Lab1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckConnection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CheckConnection
+            // 
+            this.CheckConnection.BackColor = System.Drawing.SystemColors.Info;
+            this.CheckConnection.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CheckConnection.Location = new System.Drawing.Point(315, 133);
+            this.CheckConnection.Name = "CheckConnection";
+            this.CheckConnection.Size = new System.Drawing.Size(195, 85);
+            this.CheckConnection.TabIndex = 0;
+            this.CheckConnection.Text = "Check Connection";
+            this.CheckConnection.UseVisualStyleBackColor = false;
+            this.CheckConnection.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CheckConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckConnection;
+    }
+}
+
---
+++ Second Year/Database_Management_System/Lab0/Lab1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1: Form
+    {
+        string connectionString = "Data Source=DESKTOP-VL4RVSS\\SQLEXPRESS;Initial Catalog=Navomodelism_Course;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(connectionString);
+                if (cn.State == System.Data.ConnectionState.Closed)
+                {
+                    cn.Open();
+                    MessageBox.Show("Connection opened successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
